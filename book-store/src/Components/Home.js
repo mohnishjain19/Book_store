512cc3e04eb0bc50c15ff9e8d6e9d4a413e23d30
mohnishjain19 <mohnishjain29@gmail.com>
2022-06-03 23:45:17 +0530

    second commit

--- book-store/src/Components/Home.js
+++ book-store/src/Components/Home.js
@@ -9,7 +9,7 @@ export const Home = () => {
     <div>
     <Box display="flex" flexDirecton="column" alignItems={"center"}>
     <Button LinkComponent={Link} to="/books" sx={{marginTop:25,background:' orangered',marginLeft:63} } variant="contained">
-    <Typography variant="h3">View all Products</Typography>
+    <Typography variant="h3">View all Products </Typography>
     </Button>
      
     </Box>
